fcf2d9e71d223b19d316ce4a5a3febf80f30d82a
jun9 <zhangyong@gmail.com>
2020-04-04 20:14:10 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 pip3 install -r requirements.txt
 
-mkdir -p ~/apps/Menu/54_PyGames/Arcade_template
+mkdir -p ~/apps/Menu/54_PyGames
 cp menu/*.png ~/skins/OP1/Menu/GameShell/
-cp menu/*.sh ~/apps/Menu/54_PyGames/Arcade_template/
+cp menu/*.sh ~/apps/Menu/54_PyGames/
