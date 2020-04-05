9c6a907fb94a61d88077d4e91df9a355fadd77b2
jun9 <zhangyong@gmail.com>
2020-04-05 14:33:40 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@
 
 pip3 install -r requirements.txt
 
-mkdir -p ~/apps/Menu/54_PyGames
-cp menu/*.png ~/skins/OP1/Menu/GameShell/
-cp menu/*.sh ~/apps/Menu/54_PyGames/
+cp menu/*.png ./PyGames.png 
+cp menu/*.sh ./PyGames.sh
